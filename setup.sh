19b526713836f9f9559a7e739337fb67d98f0997
Cenk Gazen <bcg@google.com>
2023-01-11 09:26:11 -0800

    Clean up.

--- setup.sh
+++ setup.sh
@@ -1,21 +1,37 @@
 #! /bin/bash
 
+# PROTOC_URL points to a protoc binary that is compatible with the protobuf
+# runtime.  swirl-lm uses the protobuf runtime that is installed by
+# tensorflow. This runtime is not compatible with the latest protoc as of
+# January 2023, which is why we explicitly download a specific version. For
+# tensorflow's current protobuf runtime version, see:
+#
+# https://github.com/tensorflow/tensorflow/blob/master/tensorflow/tools/ci_build/release/requirements_common.txt
+#
+# Tensorflow also installs protoc, which we could also try to use instead of
+# installing our own version, but that binary is not necessarily accessible to
+# the account setting up swirl-lm.
 PROTOC_URL=https://github.com/protocolbuffers/protobuf/releases/download/v3.19.6/protoc-3.19.6-linux-x86_64.zip
+
+# Path to local copy of protoc, relative to swirl-lm.
 PROTOC=protoc/bin/protoc
 
 function install_protoc() (
+  # Downloads protoc binary if it's not already downloaded.
   if [ ! -x "$PROTOC" ]; then
-    echo "Install protoc."
+    echo "Installing protoc."
     mkdir protoc
     cd protoc
     curl -L -o protoc.zip "$PROTOC_URL"
     unzip protoc.zip
   else
-    echo "Found protoc."
+    echo "Using existing protoc."
   fi
 )
 
 function run_protoc() (
+  # Generates _pb2 files in the source directories.
+  echo "Compiling proto files."
   PROTO_NAMES=$(find swirl_lm -name '*.proto')
   for proto in ${PROTO_NAMES}; do
     "$PROTOC" -I=. --python_out=. $proto
@@ -23,6 +39,7 @@ function run_protoc() (
 )
 
 function install_swirl_lm() {
+  echo "Installing swirl-lm."
   python3 -m pip uninstall -y swirl-lm
   python3 -m pip install .
 }
